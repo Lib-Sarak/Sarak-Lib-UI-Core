cf4db5e556fee1fb972bc91305374b725763fed6
Igor Sarak <igorsarak@gmail.com>
2026-04-05 03:04:22 -0300

    fix(ui-core): final color palettes and theme scroll constraints

--- src/components/ThemeCustomization/ThemeCustomizationTab.tsx
+++ src/components/ThemeCustomization/ThemeCustomizationTab.tsx
@@ -159,7 +159,7 @@ export const ThemeCustomizationTab: React.FC = () => {
                                 initial={{ opacity: 0, x: -20 }}
                                 animate={{ opacity: 1, x: 0 }}
                                 exit={{ opacity: 0, x: -20 }}
-                                className="flex-1 flex flex-col min-h-0 overflow-y-auto custom-scrollbar"
+                                className="flex-1 flex flex-col min-h-0 overflow-y-auto custom-scrollbar max-h-[calc(100vh-280px)]"
                             >
                                 <div className="p-6">
                                     <button
